b45acecc487a91f642b0fed7c181e752d8c613fd
Robin Widjeback <r.widjeback@hotmail.se>
2021-04-23 19:44:07 +0200

    homescreeen and links

--- resources/routes/get-routes.js
+++ resources/routes/get-routes.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.render("start");
     });
+
+    app.get("/all-products", (req, res) => {
+        res.render("all-products");
+    });
 };
