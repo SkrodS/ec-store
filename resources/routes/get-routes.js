8b091a082d1ab1f8907f1c515f4e1227402eb12f
Robin Widjeback <r.widjeback@hotmail.se>
2021-04-24 12:57:00 +0200

    added categories

--- resources/routes/get-routes.js
+++ resources/routes/get-routes.js
@@ -21,6 +21,28 @@ module.exports = (app, Product, Admin, Order) => {
         });
     });
 
+    app.get("/all-products/hoodies", (req, res) => {
+        Product.find({"name" : {$regex : ".*Hoodie.*"}}, [], (err, product) => {
+            if (err) {
+                res.send("error");
+            }
+            else {
+                res.render("hoodies", {product:product})
+            }
+        });
+    });
+
+    app.get("/all-products/t-shirts", (req, res) => {
+        Product.find({"name" : {$regex : ".*T-shirt.*"}}, [], (err, product) => {
+            if (err) {
+                res.send("error");
+            }
+            else {
+                res.render("t-shirts", {product:product})
+            }
+        });
+    });
+
     app.get("/product/:id", (req, res) => {
         Product.findById(req.params.id, (err, product) => {
             if (err) {
